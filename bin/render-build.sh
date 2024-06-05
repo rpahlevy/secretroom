7968f27b4d4d4c8e98598947febf9da003985a4c
Rio Pahlevy <43176811+rpahlevy@users.noreply.github.com>
2024-06-06 05:10:09 +0700

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bundle install --trace
-bundle exec rails assets:precompile --trace
-bundle exec rails assets:clean --trace
-bundle exec rails credentials:edit --trace
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails credentials:edit
