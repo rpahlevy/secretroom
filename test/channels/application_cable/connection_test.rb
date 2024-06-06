781eb6d4fc0058062e609a1e7228511ed2290350
Rio Pahlevy <43176811+rpahlevy@users.noreply.github.com>
2024-06-06 13:10:44 +0700

    add unit test

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -2,12 +2,8 @@ require "test_helper"
 
 module ApplicationCable
   class ConnectionTest < ActionCable::Connection::TestCase
-    # test "connects with cookies" do
-    #   cookies.signed[:user_id] = 42
-    #
-    #   connect
-    #
-    #   assert_equal connection.user_id, "42"
-    # end
+    test "connects without auth" do
+      assert_no_error_reported { connect }
+    end
   end
 end
--- test/channels/chat_channel_test.rb
+++ test/channels/chat_channel_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class ChatChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
+  test "subscribes" do
+    subscribe
+    assert subscription.confirmed?
+  end
+
+  test "assert transmitted message" do
+    assert_broadcast_on "secretroom_channel", { username: "testusername", usercolor: "#AAAAAA", message: "Hi!" } do
+      ActionCable.server.broadcast "secretroom_channel", { username: "testusername", usercolor: "#AAAAAA", message: "Hi!" }
+    end
+  end
 end
